d26dfff46ca437b7f4f9a63f3509488b63816df9
Jo Imlay <jo.imlay@gmail.com>
2018-05-24 16:41:15 +0100

    edit config

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
   //   DB: 'mongodb://localhost:27017/code-ninjas-test'
   // },
   dev: {
-    DB: 'https://api.mlab.com/api/1/databases?apiKey=iTfpo14AVAjGjhlnqd8_FHIVXtUJQjZQ'
+    DB: 'mongodb://dentednerd:570rm80rnMNG28@ds133630.mlab.com:33630/codeninjas'
   }
 };
