b517a1a58d712ad050213a65b4d4d3c8d60cc521
Joey Imlay <jo.imlay@gmail.com>
2021-12-06 14:19:34 +0000

    updated Procfile

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
+process.env.NODE_ENV !== 'production' && require('dotenv').config({ silent: process.env.NODE_ENV === 'production' });
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
 const firebaseConfig = {
   credential: cert({
     'projectId': process.env.FIREBASE_PROJECT_ID,
-    'private_key': process.env.FIREBASE_PRIVATE_KEY,
+    'private_key': JSON.parse(process.env.FIREBASE_PRIVATE_KEY).replace(/\\n/g, '\n'),
     'client_email': process.env.FIREBASE_CLIENT_EMAIL,
   })
 };
