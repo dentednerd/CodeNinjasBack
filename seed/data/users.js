75c54b6576255e677d0437338c8354ea0561079d
Joey Imlay <jo.imlay@gmail.com>
2023-10-23 10:17:05 +0100

    updated endpoints and users data

--- seed/data/users.js
+++ seed/data/users.js
@@ -10,5 +10,29 @@ module.exports = [
     level: 0,
     username: 'dannydontyouknow',
     avatar: 'https://pbs.twimg.com/profile_images/768900258160050177/9dKV9-AV_400x400.jpg'
+  },
+  {
+    name: 'Donatello',
+    level: 0,
+    username: 'DonatelloDoesMachines',
+    avatar: 'https://s3.amazonaws.com/comicgeeks/characters/avatars/11018.jpg'
+  },
+  {
+    name: 'Michelangelo',
+    level: 0,
+    username: 'cowabunga_dude',
+    avatar: 'https://s3.amazonaws.com/comicgeeks/characters/avatars/11013.jpg'
+  },
+  {
+    name: 'Scorpion',
+    level: 0,
+    username: 'GetOverHere92',
+    avatar: 'https://www.specfictionshop.com/cdn/shop/products/PCS10027_2000x.jpg'
+  },
+  {
+    name: 'Naruto',
+    level: 0,
+    username: 'dattebayo_naruto',
+    avatar: 'https://staticg.sportskeeda.com/editor/2022/08/53e15-16596004347246.png'
   }
 ];
